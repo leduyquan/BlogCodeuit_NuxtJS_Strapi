892e131afe72957b7edcae059b8bddcd6059cd90
Quan Duy <quanld.uit@gmail.com>
2020-09-19 21:57:39 +0700

    css spinner

--- frontend/resources/components/spinner/index.js
+++ frontend/resources/components/spinner/index.js
@@ -3,6 +3,15 @@ import './styles.scss';
 
 const Spinner = () => {
     return (
+        // <div className="loading">
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        //     <div className="loading__square"></div>
+        // </div>
         <div className="loader">Loading...</div>
     );
 };
