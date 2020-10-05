84f463b967463c7d34b1edb3b0a9636b808e78a9
Quan Duy <quan@brazn.co>
2020-10-05 09:25:29 +0700

    fix

--- frontend/pages/category.js
+++ frontend/pages/category.js
@@ -1,26 +1,23 @@
-// import { useRouter } from 'next/router';
-// import Render from '../../resources/components/render';
-// import CATEGORY_POSTS_QUERY from '../../resources/graphql/category/posts';
-// import fds from '../resources/'
+import React from 'react'
 
-// const Category = () => {
-//     const router = useRouter();
-//     return (
-//         <Render query={CATEGORY_POSTS_QUERY} path={router.query.id}>
-//             {({ data: { category } }) => {
-//                 return (
-//                     <div>
-//                         {/* <div className="uk-section">
-//                             <div className="uk-container uk-container-large">
-//                                 <h1>{category.name}</h1>
-//                                 <Body posts={category.post} />
-//                             </div>
-//                         </div> */}
-//                     </div>
-//                 );
-//             }}
-//         </Render>
-//     );
-// };
+const Category = () => {
+    return (
+        <div></div>
+        // <Render query={CATEGORY_POSTS_QUERY} path={router.query.id}>
+        //     {({ data: { category } }) => {
+        //         return (
+        //             <div>
+        //                 {/* <div className="uk-section">
+        //                     <div className="uk-container uk-container-large">
+        //                         <h1>{category.name}</h1>
+        //                         <Body posts={category.post} />
+        //                     </div>
+        //                 </div> */}
+        //             </div>
+        //         );
+        //     }}
+        // </Render>
+    );
+};
 
-// export default Category;
+export default Category;
